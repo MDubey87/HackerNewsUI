38170b488eeb6b3fe9cf4d1b4b2b49ef26f8b62e
Manish Dubey <mandeep.mca2007@gmail.com>
2024-08-09 15:19:52 -0600

    Added environment file

--- src/app/services/story.service.ts
+++ src/app/services/story.service.ts
@@ -2,14 +2,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { StoryResponseModel } from '../models/story.response.model';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StoryService {
+  private baseUrl = environment.apiBaseUrl;
   constructor(private _http: HttpClient) {}
   getStories(): Observable<StoryResponseModel> {
-    const url = 'https://localhost:5000/api/top-new-stories';
+    const url = `${this.baseUrl}/top-new-stories`;
     return this._http.get<StoryResponseModel>(url);
   }
 }
