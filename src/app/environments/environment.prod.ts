f74ff9fc0707d31ed56bde85c48fbe1c553876d9
Manish Dubey <mandeep.mca2007@gmail.com>
2024-08-13 08:19:50 -0600

    api base url to loclahost

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://hackernews.azurewebsites.net/api',
+  apiBaseUrl: 'https://localhost:5000/api',
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://hackernews.azurewebsites.net/api',
+  apiBaseUrl: 'https://localhost:5000/api',
 };
